cf7c7ca9ed712d3e4b0b97d164735ea20af01017
Alex Kong <alexkonghockey@gmail.com>
2021-11-18 07:09:48 -0800

    Set project properties and get a window to show

--- alarm/cApp.cpp
+++ alarm/cApp.cpp
@@ -1,5 +1,6 @@
 #include "cApp.h"
 
+wxIMPLEMENT_APP(cApp);
 cApp::cApp()
 {
 	
@@ -12,6 +13,8 @@ cApp::~cApp()
 
 bool cApp::OnInit()
 {
+	m_frame1 = new cMain();
+	m_frame1->Show();
 	return true;
 }
 
